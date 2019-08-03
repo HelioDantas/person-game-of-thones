d75a5be2427a2fa6478828337e5fe3522e3effe4
Helio Dantas <helio_dantas@outlook.com>
2019-08-02 22:04:02 -0300

    incluido nome sexo dinamicamente no components/CharacterCard.js e melhorado css
    Signed-off-by: Helio Dantas <helio_dantas@outlook.com>

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ import Button from './components/Button';
 function App() {
     return (
         <main className="appRoot__wrapper">
-            <CharacterCard/>
+            <CharacterCard
+                name="Jon Snow"
+                culture="Northmen"
+                gender="Male"
+                numberOfSeasons={7}
+            />
+            <CharacterCard
+                name="Daenerys"
+                culture="Westerosi"
+                gender="Female"
+                numberOfSeasons={7}
+            />
             <Button/>
         </main>
     );
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-function CharacterCard() {
+function CharacterCard(props) {
     return (
         <article className="characterCard">
             <header className="characterCard-header__wrapper --isDead">
-                <h1 className="characterCard-name">Joffrey</h1>
+                <h1 className="characterCard-name">{props.name}</h1>
             </header>
             <ul className="characterCard-body">
-                <li className="characterCard-culture">culture: Northmen</li>
-                <li className="characterCard-gender">gender: Male</li>
+                <li className="characterCard-culture">culture: {props.culture}</li>
+                <li className="characterCard-gender">gender: {props.gender}</li>
             </ul>
             <footer className="characterCard-seasonsAppearence">
-                featured in N seasons
+                featured in {props.numberOfSeasons} seasons
             </footer>
         </article>
     );
